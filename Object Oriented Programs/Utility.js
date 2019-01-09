9ba4011c7f397da72bc3e2790dc1d6f94ec8839b
sachinchouksey017 <upamanyusaha@gmail.com>
2019-01-09 19:20:29 +0530

    final oops commit

--- Object Oriented Programs/Utility.js
+++ Object Oriented Programs/Utility.js
@@ -284,7 +284,7 @@ module.exports = {
                 return this.inventoryManage(object, file);
         }
 
-
+        
     },
     /**
      * purpose: a method which returns all the card in mixing form. 
